33dc804cb5a97bfe73d32f2ae4cfbe61cb14461f
rav3ndust <35274771+rav3ndust@users.noreply.github.com>
2020-11-05 14:59:06 -0600

    Update pagegen.sh
    updated code comments

--- pagegen.sh
+++ pagegen.sh
@@ -31,30 +31,30 @@ echo "head {" >> stylesheet.css
 
 # now we ask for the basic design scheme and setup the stylesheet.css
 
-# heading background color
+# now we ask for the heading background color
 read -p "What background color would you like your heading to be? " HEADBGCOLOR	#HEADBGCOLOR will be entered into the head background-color element in stylesheet.css
 echo "Cool!"
 echo "background-color: $HEADBGCOLOR;" >> stylesheet.css
-# heading text color
+# now we ask for the heading text color
 read -p "What color would you like the text in your heading to be? " HEADTXT	#HEADTXT will entered into the stylesheet for the text color in stylesheet.css
 echo "Excellent!"
 echo "color: $HEADTXT;" >> stylesheet.css
 echo "}" >> stylesheet.css
-# body background color
+# now we ask for the body background color
 read -p "What background color would you like the body of your page to be? " BBG	#BBG will be entered into the stylesheet for the backgroundcolor in stylesheet.css
 echo "Nice!"
 echo "body {" >> stylesheet.css
 echo "background-color: $BBG;" >> stylesheet.css
-# body text color
+# now we ask for the body text color
 read -p "what color would you like the text of your page's body to be? " BTXT #BTXT will be entered into the color element in the stylesheet.css
 echo "color: $BTXT" >> stylesheet.css
 echo "}" >> stylesheet.css
-# footer background color
+# now we ask for the footer background color
 read -p "What background color would you like your footer to have? " FOOT1 #FOOT1 will be the background-color of the footer in the stylesheet.css
 echo "That's a cool choice."
 echo "footer {" >> stylesheet.css
 echo "background-color: $FOOT1;" >> stylesheet.css
-#footer text color
+# now we ask for the footer text color
 read -p "What color would you like for the text in your footer? " FOOT2 #FOOT2 will be the color element in the of the footer in the stylesheet.css
 echo "I like it."
 echo "color: $FOOT2;" >> stylesheets.css
@@ -62,35 +62,35 @@ echo "}" >> stylesheets.css
 
 # Now we can generate the basic first page with our customizations
 
-#make the heading and tab title
+# we are now able to make the heading and tab title
 echo "<head>" >> gen.html
 # ask for title of the page
 read -p "What would you like your page's title to be? " TITLE	#TITLE will be entered into the head element
 echo "Thanks!"
 
-#create the heading
+# we create the heading
 echo "<title>$TITLE</title>" >> gen.html
 echo "<h1>Generated Site</h1>" >> gen.html
 echo "</head>" >> gen.html
 
-# make the body
+# we make the body
 echo "<body>" >> gen.html
 echo "<p>Placeholder content.</p>" >> gen.html
 echo "</body>" >> gen.html
 
-#make the footer
+# we make the footer
 echo "<footer>" >> gen.html
 echo "<p>Placeholder content.</p>" >> gen.html
 echo "</footer>" >> gen.html
 
-# close the html 
+# we close the html 
 echo "</html>" >> gen.html
 
-#notify the user they are finished
+# now we can notify the user that the script has finished
 echo "Your file is done. Don't forget to rename it and add in your content!"
 notify-send -u critical "Pages Generated." "Your HTML and CSS files have been created."
 
-# navigate back to the home directory and display the files on the terminal
+# now we can navigate back to the home directory and display the files on the terminal
 cd && cd www && ls
 echo "Here are your files."
 notify-send "Files Displaying in Terminal" "Now you can edit them to your liking."
